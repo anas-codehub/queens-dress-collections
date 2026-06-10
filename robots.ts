51a9e20df6eee3cf230d675c61d486db82ad0180
Talat Mahmud Anas <talatmahmudanas002025@gmail.com>
2026-06-10 23:50:22 +0600

    push

--- robots.ts
+++ robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next"
 
-const BASE_URL = process.env.NEXT_PUBLIC_APP_URL ?? "https://queensdresscollection.com"
+const BASE_URL = process.env.NEXT_PUBLIC_APP_URL ?? "https://queensdress.com"
 
 export default function robots(): MetadataRoute.Robots {
   return {
